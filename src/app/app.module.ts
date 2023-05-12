c9ea6674e5a208fa6e03343b963975dd7e461d13
fayis <muhammed7fayis77@gmail.com>
2023-05-12 16:53:22 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './new/layout/header/header.component';
+import { FooterComponent } from './new/layout/footer/footer.component';
+import { NewModule } from './new/new.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [       
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
